10055fe3165e1040e0dca26ab980205f4e76f91d
g12953tk <g12953tk+git@s30431.tsuda.ac.jp>
2014-05-09 11:35:46 +0900

    5-1

---
+++ NumEnglish/src/numenglish/NumEnglish.java
@@ -0,0 +1,16 @@
+package numenglish;
+
+import java.util.Scanner;
+
+public class NumEnglish {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		System.out.println(translateEng(input));
+		// TODO Auto-generated method stub
+int n=0;
+
+	}
+
+}
